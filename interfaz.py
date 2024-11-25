1d939e8703089ae777166a95a228863f72e10b09
Conor404 <conorjose1234@gmail.com>
2024-11-24 20:06:15 -0400

    primer commit de la interfaz

--- interfaz.py
+++ interfaz.py
@@ -1,36 +1,35 @@
 # PARA QUE LES FUNCIONE TIENEN QUE INSTALAR LA LIBRERIA CON -pip install customtkinter-
-from customtkinter  import CTk, CTkFrame, CTkEntry, CTkLabel,CTkButton,CTkCheckBox
-from tkinter import PhotoImage
+import customtkinter as ctk 
+import os
+from PIL import ImageTk, Image
 
-root = CTk() 
-root.geometry("500x600+350+20")
-root.minsize(480,500)
-root.config(bg ='#010101')
-root.title("Inicio de Sesión")
+carpeta_principal = os.path.dirname(__file__)
+carpeta_imagenes = os.path.join(carpeta_principal,"Imagenes")
 
-frame = CTkFrame(root, fg_color='#010101')
-frame.grid(column=0, row = 0, sticky='nsew',padx=50, pady =50)
-
-frame.columnconfigure([0,1], weight=1)
-frame.rowconfigure([0,1,2,3,4,5], weight=1)
-
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
-
-correo = CTkEntry(frame, placeholder_text='Usuario', border_color='#2cb67d', fg_color='#010101', width=220, height=40)
-correo.grid(columnspan=2, row=1,padx=4, pady =4)
-
-contrasenna = CTkEntry(frame,show="*", placeholder_text= 'Contraseña',
- border_color='#2cb67d', fg_color= '#010101', width =220,height=40)
-contrasenna.grid(columnspan=2, row=2,padx=4, pady =4)
-
-checkbox = CTkCheckBox(frame, text="Recuerdame",hover_color='#7f5af0', 
-	border_color='#2cb67d', fg_color='#2cb67d')
-checkbox.grid(columnspan=2, row=3,padx=4, pady =4)
-
-bt_iniciar = CTkButton(frame, border_color='#2cb67d', fg_color='#010101',
-	hover_color='#2cb67d',corner_radius=12,border_width=2,
-    text='Entrar')
-bt_iniciar.grid(columnspan=2, row=4,padx=4, pady =4)
-
-root.mainloop()
+class Ventana:
+    def __init__(self):
+        self.app =ctk.CTk()
+        self.app.geometry("1000x500")
+        self.app.title("Ventus Novus")
+        self.app.iconbitmap(os.path.join(carpeta_imagenes,"logoSimple.ico"))
+        self.app.resizable(False,False)
+        
+        barra = ctk.CTkFrame(master=self.app, width=50, height=50, fg_color="black")
+        barra.pack(side="top", fill="x", padx= 100)
+        
+        logoV = ctk.CTkImage(
+			light_image= Image.open((os.path.join(carpeta_imagenes,"logoOpaco.png"))),
+			size=(250,250)
+		)
+        
+        etLogoV = ctk.CTkLabel(master = self.app, image= logoV, text="")
+        
+        etLogoV.pack(pady = 100)
+        
+        
+        
+        self.app.mainloop()
+        
+        
+        
+app = Ventana()
